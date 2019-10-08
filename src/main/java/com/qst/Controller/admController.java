91d710ef079a6e14db49ef779d732462a742e46f
SexJava <man021436@163.com>
2019-10-08 15:56:49 +0800

    Complete the classification login function 完成分类登录功能

--- src/main/java/com/qst/Controller/admController.java
+++ src/main/java/com/qst/Controller/admController.java
@@ -1,11 +1,6 @@
 package com.qst.Controller;
 
-import com.qst.Service.admService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.annotation.Resource;
-
 /**
  * @author LiuYunDa
  * @date 2019/10/8 - 14:57
